571e23f844d344a0a5e75fe440f8dd30d3a895a5
Marmot <Marmot@Marmo-Mathghamhain.local>
2018-10-12 15:15:17 -0500

    API call and insertion to forecasts page working for custom city! :~)

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ window.onload = function populateForecasts(){
 		// $(modal + ' > h2').html();
 		// $(modal + ' > h2').html();
 		
-		console.log(data);
+		// console.log(data);
 
 		// Increase counter
 		i += 1;
@@ -43,6 +43,35 @@ window.onload = function populateForecasts(){
 // Custom city form input & API call
 
 function customCity() {
-  var city = document.getElementById('customCityInput').value;
-  document.getElementById('alert').innerHTML = 'The user input is: ' + city;
+
+	// Get user input
+	var city = document.getElementById('customCityInput').value;
+
+	// Make API request
+	var request = new XMLHttpRequest();
+	request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ad2f4ea29134ad456296567cc7fbb4ba", false);
+	request.send(null);
+	var data = JSON.parse(request.responseText);
+	if (data.message == 'city not found') {
+		$('#customCityError').html("City not found 🤢");
+		return;
+	}
+
+	// convert K to F
+	var temp = Math.round((data.main.temp - 273.15) * 9/5 + 32);
+
+	$('#cityCustomCity').html(city);
+	$('#tempCustomCity').html(temp + '&deg;');
+	$('#iconCustomCity').html("<img src=http://openweathermap.org/img/w/10d.png>");
+
+	// JSON insertions for modals
+	// $(modalCustomCity + ' > h2').html(city + " Weather");
+	// $(modalCustomCity + ' > p:nth-child(2)').html(temp + '&deg;' + " with " + data.weather[0].description);
+	// $(modalCustomCity + ' > p:nth-child(3)').html(data.wind.speed + " mph winds");
+	// $(modalCustomCity + ' > p:nth-child(4)').html(data.main.humidity + "% humidity");
+
+	console.log(data);
 }
+
+
+
